c56fef69495de4702502cbf203c720434e2ce393
jurchiks33 <jurisslisans@gmail.com>
2023-10-12 19:55:22 +0200

    fixing this.

--- sketch.js
+++ sketch.js
@@ -169,7 +169,7 @@ function ChocolateBar(name, imageFile, weight, price, display) {
 	//draw the chocolate bars image to the values set in the display object
 	//also display the price and price per 100grams
 	this.draw = function() {
-		image(image, this.display.x, this.display.y, this.display.width,
+		image(this.image, this.display.x, this.display.y, this.display.width,
 			this.display.height);
 		var pricePer100g = price / weight * 100;
 		var priceString = "£" + price + "p ( £" + pricePer100g +
